8a2aaa8f544689eaaadd2f3ce44e2b41ec901fdd
unknown <navdeep220161@gmail.com>
2026-09-15 12:56:56 +0530

    status

--- lab3_multi_agents/part3_4_graph_agent/agent.py
+++ lab3_multi_agents/part3_4_graph_agent/agent.py
@@ -48,6 +48,7 @@ def save_original_request(ctx: Context, node_input: str) -> str:
     """
 
     ctx.state["original_request"] = node_input
+    ctx.state["symbol"]="NQ"
 
     return node_input
 
@@ -250,8 +251,8 @@ root_agent = Workflow(
         # Preserve the original request first
         (
             "START",
-            save_original_request,
-            request_classifier,
+            product_support_agent,
+            market_status_support_agent,
             route_request,
         ),
 
